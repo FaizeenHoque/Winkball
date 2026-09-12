1d5fd42ebb308a0d6b02dc6137e2ac22b22a6d19
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-09-13 01:41:55 +0600

    Added Cascade, Loaded Cascade and Reimplemented Camera

--- CV/main.cpp
+++ CV/main.cpp
@@ -1,16 +1,35 @@
-#include <opencv5/opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/xobjdetect.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/utility.hpp>
 #include <iostream>
 
+cv::CascadeClassifier cascade;
+
+void detectAndDisplay(cv::Mat& frame) {}   
+
 int main() {
-    cv::VideoCapture cap(0);
-    if (!cap.isOpened()) { std::cerr << "Error: Could not open camera." << std::endl; return -1;}
+    // Load Cascade
+    std::string cascade_name = cv::samples::findFile("haarcascade_frontalface_default.xml");
+    if (!cascade.load(cascade_name)) {
+        std::cout << "Error: Could not load cascade classifier." << std::endl;
+        return -1;
+    }
 
-    std::cout << "Camera opened successfully!" << std::endl;
+    // Load Camera
+    cv::VideoCapture capture;
+    capture.open(0);
+    if (!capture.isOpened()) {
+        std::cout << "Error: Could not open camera." << std::endl;
+        return -1;
+    }
+    
     cv::Mat frame;
-
-    while (true) {
-        cap >> frame;
+    while (capture.read(frame)) {
         if (frame.empty()) { std::cerr << "Error: Empty frame." << std::endl; break;}
+
+        detectAndDisplay(frame);
         
         cv::imshow("Wink Ball Camera", frame);
         if (cv::waitKey(1) == 27) { break; }
